d9c2fa024ab6abf21c65188350adae55e6ffde09
pelatge <pelat.g29@gmail.com>
2023-01-05 16:21:34 +0700

    Updates

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+  'index.html',
+  ],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      },
+    },
+  plugins: [
+   // include Flowbite as a plugin in your Tailwind CSS project
+   // require('flowbite/plugin')
+  ],
+};
